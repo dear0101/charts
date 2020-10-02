2ce2a7d38789c16470d82214600ca4e3999926c9
dear0101 <1424505313@qq.com>
2020-10-02 11:35:44 +0800

    顶部组件初始化

---
+++ src/components/BottomView/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    bottomview
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TopView/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="TopView">
+    top
+</div>
+</template>
+
+<script>
+export default {
+    name: 'TopView'
+}
+</script>
+
+<style>
+
+</style>
