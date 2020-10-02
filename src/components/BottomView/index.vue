8933f2d286f25fb571f0e008893f9ad668f441a3
dear0101 <1424505313@qq.com>
2020-10-02 23:56:26 +0800

    销售e图表组件头部

--- src/components/BottomView/index.vue
+++ src/components/BottomView/index.vue
@@ -1,33 +1,13 @@
 <template>
 <div>
-    <ve-line :data='vchartData'>
-    </ve-line>
+    btoom
 </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {
-            vchartData: {
-                columns: ['日期', '销售额'],
-                rows: [{
-                        '日期': '1月1号',
-                        '销售额': 123
-                    },
-                    {
-                        '日期': '1月2号',
-                        '销售额': 123
-                    }, {
-                        '日期': '1月3号',
-                        '销售额': 123
-                    }, {
-                        '日期': '1月4号',
-                        '销售额': 123
-                    }
-                ]
-            }
-        }
+        return {}
     },
 }
 </script>
--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,7 +1,27 @@
 <template>
-<div>
-    <v-chart :options="chartData">
-    </v-chart>
+<div class="sales-view">
+    <el-card shadow="hover">
+        <template v-slot:header>
+            <div class="menu-wrapper">
+                <el-menu class="sales-view-el-menu" mode="horizontal" :default-active="activeIndex" @select="onMenuSelect">
+                    <el-menu-item index="1">销售额</el-menu-item>
+                    <el-menu-item index="2">访问量</el-menu-item>
+                </el-menu>
+                <div class="menu-right">
+                    <el-radio-group v-model="radioSelect" size="small">
+                        <el-radio-button label="今日"></el-radio-button>
+                        <el-radio-button label="明日"></el-radio-button>
+                        <el-radio-button label="今年"></el-radio-button>
+                        <el-radio-button label="明年"></el-radio-button>
+                    </el-radio-group>
+                    <el-date-picker class="data-picker" unlink-panels type="daterange" v-model="dateData" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" size='small' :picker-options="pickerOptions"></el-date-picker>
+                </div>
+            </div>
+        </template>
+        <template>
+            <div>222</div>
+        </template>
+    </el-card>
 </div>
 </template>
 
@@ -9,22 +29,80 @@
 export default {
     data() {
         return {
-            chartData: {
-                xAxis: {
-                    type: 'category'
-                },
-                yAxis: {},
-                series: [{
-                    type: 'line',
-                    data: [100, 200, 300]
-                }]
+            activeIndex: '1',
+            radioSelect: '今日',
+            dateData: null,
+            pickerOptions: {
+                shortcuts: [{
+                        text: '最近一周',
+                        onClick(picker) {
+                            const start = new Date()
+                            const end = new Date()
+                            //往回算7天,改变start
+                            start.setTime(start.getTime() - 3600 * 24 * 1000 * 7)
+                            picker.$emit('pick', [start, end])
+                        }
+                    },
+                    {
+                        text: '最近一个月',
+                        onClick(picker) {
+                            const start = new Date()
+                            const end = new Date()
+                            //往回算7天,改变start
+                            start.setTime(start.getTime() - 3600 * 24 * 1000 * 30)
+                            picker.$emit('pick', [start, end])
+                        }
+                    },
+                    {
+                        text: '最近三个月',
+                        onClick(picker) {
+                            const start = new Date()
+                            const end = new Date()
+                            //往回算7天,改变start
+                            start.setTime(start.getTime() - 3600 * 24 * 1000 * 90)
+                            picker.$emit('pick', [start, end])
+                        }
+                    }
+                ],
             }
-
+        }
+    },
+    methods: {
+        onMenuSelect(index) {
+            this.activeIndex = index
         }
     }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.menu-wrapper {
+    display: flex;
+    position: relative;
 
+    .sales-view-el-menu {
+        width: 100%;
+        padding-left: 20px;
+
+        .el-menu-item {
+            height: 50px;
+            line-height: 50px;
+            margin: 0 20px;
+        }
+    }
+
+    .menu-right {
+        position: absolute;
+        top: 0;
+        right: 20px;
+        height: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+
+        .data-picker {
+            margin-left: 20px;
+        }
+    }
+}
 </style>
